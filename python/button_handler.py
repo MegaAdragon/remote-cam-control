fff02a4e1a397176f9af1a3006ba046c874bc303
MegaAdragon <dominik.zipperle@googlemail.com>
2021-05-28 01:12:01 +0200

    Add ButtonHandler to handle interaction with Touch pHAT -> save and recall position
    Implement interaction with PIMORONI Touch pHat. Long press on pad to save the current position. Short press on pad to go to the saved position.

---
+++ python/button_handler.py
@@ -0,0 +1,72 @@
+import time
+
+
+class ButtonHandler:
+    def __init__(self, keys):
+        self._buttons = []
+        for k in keys:
+            self._buttons.append({'key': k, 'state': 'Released'})
+
+    def on_press(self, key, handler=None):
+        if handler is None:
+            def decorate(handler):
+                self._bind_handler('on_press', key, handler)
+
+            return decorate
+        self._bind_handler('on_press', key, handler)
+
+    def on_long_press(self, key, handler=None):
+        if handler is None:
+            def decorate(handler):
+                self._bind_handler('on_long_press', key, handler)
+
+            return decorate
+        self._bind_handler('on_long_press', key, handler)
+
+    def _bind_handler(self, name, key, handler):
+        if type(key) == list:
+            for k in key:
+                button = self._get_button(k)
+                if button is not None:
+                    button[name] = handler
+        else:
+            button = self._get_button(key)
+            if button is not None:
+                button[name] = handler
+
+    def _get_button(self, key):
+        if len(self._buttons) < 1:
+            return None
+        for button in self._buttons:
+            if button['key'] is key:
+                return button
+        return None
+
+    def on_pressed(self, key):
+        button = self._get_button(key)
+        if button is None:
+            return
+
+        if button['state'] == 'Released':
+            button['state'] = 'Start'
+            button['pressStart'] = time.time()
+
+    def on_released(self, key):
+        button = self._get_button(key)
+        if button is None:
+            return
+        button['state'] = 'Released'
+
+    def process(self):
+        current_time = time.time()
+        for button in self._buttons:
+            if button['state'] == 'Start':
+                if current_time - button['pressStart'] > 0.2:
+                    button['state'] = 'Pressed'
+                    if 'on_press' in button:
+                        button['on_press'](button['key'])
+            elif button['state'] == 'Pressed':
+                if current_time - button['pressStart'] > 1.5:
+                    button['state'] = 'LongPressed'
+                    if 'on_long_press' in button:
+                        button['on_long_press'](button['key'])
--- python/camcontrol.py
+++ python/camcontrol.py
@@ -1,7 +1,12 @@
 import socket
 import time
 import pygame
-from pygame import K_DOWN, K_UP, K_LEFT, K_RIGHT, K_h
+import button_handler
+
+try:
+    import touchphat
+except ImportError:
+    print("Touch PHAT not supported")
 
 HOST = '192.168.0.117'  # The server's hostname or IP address
 PORT = 80  # The port used by the server
@@ -14,9 +19,13 @@ def handle_recv(data):
     data = data.split(b'\xFF')
     for msg in data:
         if len(msg) == 18 and msg[0] == 0x01 and msg[1] == 0x0A:
-            panPos = int.from_bytes(bytearray([msg[2] + msg[3], msg[4] + msg[5], msg[6] + msg[7], msg[8] + msg[9]]), byteorder='little', signed=True)
-            tiltPos = int.from_bytes(bytearray([msg[10] + msg[11], msg[12] + msg[13], msg[14] + msg[15], msg[16] + msg[17]]), byteorder='little', signed=True)
+            panPos = int.from_bytes(bytearray([msg[2] + msg[3], msg[4] + msg[5], msg[6] + msg[7], msg[8] + msg[9]]),
+                                    byteorder='little', signed=True)
+            tiltPos = int.from_bytes(
+                bytearray([msg[10] + msg[11], msg[12] + msg[13], msg[14] + msg[15], msg[16] + msg[17]]),
+                byteorder='little', signed=True)
             print("pos:", panPos, tiltPos)
+            return [0x01, 0x0A], panPos, tiltPos
 
 
 def encode_stepper_pos(data, pos):
@@ -33,35 +42,48 @@ joystick = None
 req_pan_speed = 0
 req_tilt_speed = 0
 
+bHandler = button_handler.ButtonHandler(['A', 'B', 'C', 'D', 'Back'])
+
+joystick_key_map = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+}
+
+joystick_button_state = {
+    'A': False,
+    'B': False,
+    'C': False,
+    'D': False
+}
+
+
 def handle_joystick():
-    pan_speed = 0
-    tilt_speed = 0
-
     global joystick
+    if joystick is None:
+        return
 
-    if joystick is not None:
-        x_axis = joystick.get_axis(0)
-        y_axis = -joystick.get_axis(1)
+    x_axis = joystick.get_axis(0)
+    y_axis = -joystick.get_axis(1)
 
-        if abs(x_axis) < 0.05:
-            x_axis = 0
-        if abs(y_axis) < 0.05:
-            y_axis = 0
+    if abs(x_axis) < 0.05:
+        x_axis = 0
+    if abs(y_axis) < 0.05:
+        y_axis = 0
 
-        pan_speed = round(0x7F * x_axis)
-        tilt_speed = round(0x7F * y_axis)
+    pan_speed = round(0x7F * x_axis)
+    tilt_speed = round(0x7F * y_axis)
 
-    pressed_keys = pygame.key.get_pressed()
-    if pressed_keys[K_UP]:
-        tilt_speed = 0x7F
-    elif pressed_keys[K_DOWN]:
-        tilt_speed = -0x7F
-    if pressed_keys[K_LEFT]:
-        pan_speed = -0x7F
-    elif pressed_keys[K_RIGHT]:
-        pan_speed = 0x7F
-
-    pygame.draw.circle(screen, (255, 0, 0), (400 + pan_speed * (200 / 0x7F) , 300 - tilt_speed * (200 / 0x7F)), 10)
+    for key in joystick_key_map:
+        if joystick.get_button(joystick_key_map[key]):
+            touchphat.set_led(key, True)
+            joystick_button_state[key] = True
+            bHandler.on_pressed(key)
+        elif joystick_button_state[key] == True:    # button was pressed
+            touchphat.set_led(key, False)
+            joystick_button_state[key] = False
+            bHandler.on_released(key)
 
     global req_pan_speed
     global req_tilt_speed
@@ -88,11 +110,81 @@ def init_joystick():
     print("Number of axes: " + str(joystick.get_numaxes()))
 
 
+sock = None
+storedPositions = {}
+
+
+@touchphat.on_touch(['Back', 'A', 'B', 'C', 'D'])
+def handle_touch(event):
+    bHandler.on_pressed(event.name)
+
+
+@touchphat.on_release(['Back', 'A', 'B', 'C', 'D'])
+def handle_release(event):
+    bHandler.on_released(event.name)
+
+
+@bHandler.on_press(['A', 'B', 'C', 'D'])
+def handle_press(key):
+    print("on_press", key)
+    if key in storedPositions:
+        print("Go to stored position", key, storedPositions[key][0], storedPositions[key][1])
+        data = bytearray([0x01, 0x01])
+        encode_stepper_pos(data, storedPositions[key][0])
+        encode_stepper_pos(data, storedPositions[key][1])
+        data.append(0xFF)
+        s.sendall(data)
+
+
+@bHandler.on_long_press(['A', 'B', 'C', 'D'])
+def handle_long_press(key):
+    print("on_long_press", key)
+
+    # fast blink
+    for i in range(0, 5):
+        touchphat.set_led(key, True)
+        time.sleep(0.1)
+        touchphat.set_led(key, False)
+        time.sleep(0.1)
+
+    # poll the current axis position
+    s.sendall(bytearray([0x01, 0x0A, 0xFF]))
+
+    # TODO: use timeout
+    while True:
+        try:
+            data = s.recv(32)
+            result = handle_recv(data)
+            if result is not None and result[0][0] == 0x01 and result[0][1] == 0x0A:
+                print("store", key, result[1], result[2])
+                storedPositions[key] = [result[1], result[2]]
+                break
+        except socket.error:
+            '''no data yet..'''
+
+
+@bHandler.on_long_press(['Back'])
+def handle_restart(key):
+    print("request restart")
+    # TODO: reset everything
+
+
 if __name__ == '__main__':
     init_joystick()
+
+    pads = ['Back', 'A', 'B', 'C', 'D', 'Enter']
+    for pad in pads:
+        touchphat.set_led(pad, True)
+        time.sleep(0.1)
+    time.sleep(0.2)
+    for pad in pads[::-1]:
+        touchphat.set_led(pad, False)
+        time.sleep(0.1)
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         s.setblocking(False)
+        sock = s
 
         running = True
         while running:
@@ -104,19 +196,10 @@ if __name__ == '__main__':
             pygame.draw.circle(screen, (255, 0, 0), (400, 300), 200, 1)
 
             handle_joystick()
-
-            pressed_keys = pygame.key.get_pressed()
-            if pressed_keys[K_h]:
-                data = bytearray([0x01, 0x01])
-                encode_stepper_pos(data, 100)
-                encode_stepper_pos(data, 200)
-                data.append(0xFF)
-                s.sendall(data) # set pan + tilt to 0
+            bHandler.process()
 
             # poll the current axis position
             s.sendall(bytearray([0x01, 0x0A, 0xFF]))
-
-            pygame.display.flip()
 
             try:
                 data = s.recv(32)
